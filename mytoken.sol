a529db2971261a9e5997dd480af75754d00c22fc
sarin-madhav <sarinmadhav0@gmail.com>
2023-11-26 01:06:10 +0530

    Metacrafters project

---
+++ mytoken.sol
@@ -0,0 +1,31 @@
+
+pragma solidity 0.8.18;
+
+
+
+contract MyToken {
+
+    
+    string public tokenName = "TIPPU";
+    string public tokenAbbrv = "TPU";
+    uint public totalSupply = 0;
+
+        
+        mapping(address => uint) public balances;
+
+
+        
+    function mint (address _address,uint _value)public {
+        totalSupply += _value;
+        balances[_address] += _value;
+        }
+        
+    function burn (address _address, uint _value) public{
+            if (balances[_address] >= _value) {
+
+                totalSupply -= _value;
+                balances[_address] -= _value;
+            }
+    }
+
+}
